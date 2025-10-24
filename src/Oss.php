6c7738a8122620c8f226c8deebc5bc9cdfd82351
酷曦科技 <admin@kuxi.tech>
2025-10-24 15:05:14 +0800

    修复OSS无法使用的问题
    Signed-off-by: 酷曦科技 <admin@kuxi.tech>

--- src/Oss.php
+++ src/Oss.php
@@ -4,27 +4,23 @@ declare(strict_types=1);
 
 namespace think\filesystem\driver;
 
-
-use League\Flysystem\AdapterInterface;
+use League\Flysystem\FilesystemAdapter;
 use think\filesystem\Driver;
 use Xxtime\Flysystem\Aliyun\OssAdapter;
-
 
 class Oss extends Driver
 {
     /**
-     * @return AdapterInterface
+     * @return FilesystemAdapter
      * @throws \Exception
-     *
      */
-    protected function createAdapter(): AdapterInterface
+    protected function createAdapter(): FilesystemAdapter
     {
         $config = [
-            'bucket'   => $this->config['bucket'],
+            'bucket'   => $this->config['bucket']，
             'endpoint' => $this->config['endpoint'],
         ];
         if (empty($this->config['credentials'])) {
-            //使用 函数计算 中的 credentials
             $config['accessId']     = $this->config['accessKeyID'];
             $config['accessSecret'] = $this->config['accessKeySecret'];
             $config['token']        = $this->config['securityToken'];
@@ -33,6 +29,8 @@ class Oss extends Driver
             $config['accessSecret'] = $this->config['credentials']['accessSecret'] ?? '';
         }
 
+        // 注意：确保返回的 OssAdapter 实现了 League\Flysystem\FilesystemAdapter（v2）。
+        // 如果 Xxtime\Flysystem\Aliyun\OssAdapter 仍是 v1 适配器，需要替换为 v2 版或使用兼容包装器。
         return new OssAdapter($config);
     }
 }
